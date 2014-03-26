e1d25c447a9094be9d84b0b841f644a01d4726fb
Venciity <venciity@abv.bg>
2014-03-26 19:51:36 +0200

    CLOCK

---
+++ PROGRAMMING/PART 1/Clock/Clock/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //int hour = 0;
+            //int minute = 0;
+            //string time = "";
+
+            //do
+            //{
+            //    Console.WriteLine("Въведете час във формат чч:мм");
+            //    string timeString = Console.ReadLine();
+
+            //    string[] elements = timeString.Split(':');
+                
+            //    if (elements.Length != 2)
+            //        continue;
+
+            //    hour = Int32.Parse(elements[0]);
+            //    minute = Int32.Parse(elements[1]);
+            //} while (hour < 0 || hour > 23 || minute < 0 || minute > 59);
+
+            while (true)
+            {
+                Console.Clear();
+                DateTime currentTime = DateTime.Now;
+
+                string hourString = ConvertTimeUnitsToString(currentTime.Hour, true);
+                string minuteString = ConvertTimeUnitsToString(currentTime.Minute, false);
+                string secondString = ConvertTimeUnitsToString(currentTime.Second, false);
+
+                Console.WriteLine(hourString + " часа и "
+                    + minuteString + (currentTime.Minute == 1 ? " минута" : " минути")
+                    + " и " + secondString + (currentTime.Second == 1 ? " секунда" : " секунди"));
+
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+
+        //Converts time units (hours, minutes or seconds) to word
+        private static string ConvertTimeUnitsToString(int unit, bool isHour)
+        {
+            string time = "";
+
+            int ones = unit % 10;
+            int tens = unit / 10;
+
+            string unitOnesString = ConvertDigitToString(ones, isHour);
+            string unitOnesMasculineString = ConvertDigitToString(ones, true);
+            string unitTensString = ConvertDigitToString(tens, true);
+
+            if (tens == 0) // 1 2 3 4 5 6 7 8 9
+                time += unitOnesString;
+            else if (tens == 1) //1x
+            {
+                if (ones == 0) //10
+                {
+                    //do nothing
+                }
+                else if (ones == 1) //11
+                    time += unitOnesMasculineString + "а";
+                else //12 13 14 15 16 17 18 19
+                    time += unitOnesMasculineString + "на";
+
+                time += "десет";
+            }
+            else
+            {
+                if (ones == 0) //10 20 30 40 50
+                    time += unitTensString + "десет";
+                else //21 22 ... 28 29 31 32 33 ... 39 41 42 ... 48 49 51 ... 59
+                    time += unitTensString + "десет и " + unitOnesString;
+            }
+
+            return time;
+        }
+
+        private static string ConvertDigitToString(int digit, bool masculine)
+        {
+            switch (digit)
+            {
+                case 0:
+                    return "нула";
+                case 1:
+                    return masculine ? "един" : "една";
+                case 2:
+                    return masculine ? "два" : "две";
+                case 3:
+                    return "три";
+                case 4:
+                    return "четири";
+                case 5:
+                    return "пет";
+                case 6:
+                    return "шест";
+                case 7:
+                    return "седем";
+                case 8:
+                    return "осем";
+                case 9:
+                    return "девет";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ PROGRAMMING/PART 1/Clock2/Clock2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.Clear();
+                DateTime currentTime = DateTime.Now;
+                int hour = currentTime.Hour;
+                int minute = currentTime.Minute;
+                int second = currentTime.Second;
+
+                PrintPart(true, hour);
+                
+                Print("часа,", true);
+                PrintPart(false, minute);
+                Print("минути и", true);
+                PrintPart(false, second);
+                Print("секунди", true);
+
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+
+        private static void PrintPart(bool isHour, int unit)
+        {
+            int partOnes = unit % 10;
+            int partTens = unit / 10;
+
+            //00
+            Print(ConvertDigitToString(0, isHour), partOnes == partTens && partTens == 0);
+
+            for (int i = 1; i <= 9; i++)
+            {
+                bool highlight = 
+                    //11 12 13 14 15 16 17 18 19
+                    (partTens == 1 && i == partOnes) ||
+                    //20 21 .. 58 59
+                    (partTens > 1 && i == partTens);
+
+                Print(ConvertDigitToString(i, true), highlight);
+                
+            }
+
+            Console.WriteLine();
+
+            Print("н", partTens == 1 && partOnes > 1); //12 13 14 15 16 17 18 19
+            Print("а", partTens == 1 && partOnes > 0); //11 12 13 14 15 16 17 18 19
+
+            //10 11 12 .... 59
+            Print("десет", partTens > 0); 
+
+            // 21 22 23 ... 29 31 32 .. 39 41 42 .. 49 51 52 .. 59
+            Print("и", partTens > 1 && partOnes != 0);
+
+            for (int i = 1; i <= 9; i++)
+            {
+                bool highlight =
+                    //1 2 3 4 5 6 7 8 9
+                    (partTens == 0 && i == partOnes) ||
+                    // 21 22 23 ... 29 31 32 .. 39 41 42 .. 49 51 52 .. 59
+                    (partTens > 1 && i == partOnes); 
+
+                Print(ConvertDigitToString(i, isHour), highlight);
+            }
+            Console.WriteLine();
+
+        }
+
+        private static void Print(string text, bool highlight)
+        {
+            if (highlight)
+                Console.ForegroundColor = ConsoleColor.White;
+            else
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.Write(text);
+        }
+
+        private static string ConvertDigitToString(int digit, bool masculine)
+        {
+            switch (digit)
+            {
+                case 0:
+                    return "нула";
+                case 1:
+                    return masculine ? "един" : "една";
+                case 2:
+                    return masculine ? "два" : "две";
+                case 3:
+                    return "три";
+                case 4:
+                    return "четири";
+                case 5:
+                    return "пет";
+                case 6:
+                    return "шест";
+                case 7:
+                    return "седем";
+                case 8:
+                    return "осем";
+                case 9:
+                    return "девет";
+                default:
+                    return "";
+            }
+        }
+    }
+}
