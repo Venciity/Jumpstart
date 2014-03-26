d96a997c9046522ff68c802c4597a0109903eb0f
Venciity <venciity@abv.bg>
2014-03-26 19:52:08 +0200

    Conditional-Statements-Demos

---
+++ PROGRAMMING/PART 1/Conditional-Statements-Demos/MultipleIfElse/MultipleIfElse.cs
@@ -0,0 +1,38 @@
+using System;
+
+class MultipleIfElse
+{
+    static void Main()
+    {
+        Console.Write("Please enter a character: ");
+        char ch = Console.ReadKey().KeyChar;
+
+        Console.WriteLine();
+
+		if (ch == 'A' || ch == 'a')
+		{
+			Console.WriteLine("Vowel [ei]");
+		}
+		else if (ch == 'E' || ch == 'e')
+		{
+			Console.WriteLine("Vowel [i:]");
+		}
+		else if (ch == 'I' || ch == 'i')
+		{
+			Console.WriteLine("Vowel [ai]");
+		}
+		else if (ch == 'O' || ch == 'o')
+		{
+			Console.WriteLine("Vowel [ou]");
+		}
+		else if (ch == 'U' || ch == 'u')
+		{
+			Console.WriteLine("Vowel [ju:]");
+		}
+		else
+		{
+			Console.WriteLine("Consonant");
+		}
+
+    }
+}
