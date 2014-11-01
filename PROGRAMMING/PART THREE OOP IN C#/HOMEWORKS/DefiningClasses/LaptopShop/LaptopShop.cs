9cc59625caadfdffa0df2a3e7ed853e0d4ebdbdb
Ventsislav Yordanov <venciity@abv.bg>
2014-11-02 00:39:48 +0200

    remove comments

--- PROGRAMMING/PART THREE OOP IN C#/HOMEWORKS/DefiningClasses/LaptopShop/LaptopShop.cs
+++ PROGRAMMING/PART THREE OOP IN C#/HOMEWORKS/DefiningClasses/LaptopShop/LaptopShop.cs
@@ -1,11 +1,6 @@
 ﻿namespace LaptopShop
 {
     using System;
-
-    //using System.Collections.Generic;
-    //using System.Linq;
-    //using System.Text;
-    //using System.Threading.Tasks;
     class LaptopShop
     {
         static void Main()
