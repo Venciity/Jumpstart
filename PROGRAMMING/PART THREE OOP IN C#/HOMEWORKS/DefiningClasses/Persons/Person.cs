58768d7d2ee5c79ba48f6c49f35efc6a344f8d6b
Ventsislav Yordanov <venciity@abv.bg>
2014-11-01 17:15:29 +0200

    Defining classes task 1

---
+++ PROGRAMMING/PART THREE OOP IN C#/HOMEWORKS/DefiningClasses/Persons/Person.cs
@@ -0,0 +1,77 @@
+﻿namespace Persons
+{
+    using System;
+    class Person
+    {
+        private string name;
+        private byte age;
+        private string address;
+
+        public Person(string name, byte age, string address = null)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Address = address;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name", "Name caanot be empty or null");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public byte Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < 1 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("Age", "Age must be in range: [1...120]");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return this.address;
+            }
+
+            set
+            {
+                if (value != null && !value.Contains("Town"))
+                {
+                    throw new ArgumentException("Address", "Address must contains \"Town\"");
+                }
+
+                this.address = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0}, Age:{1}", this.Name, this.Age) +
+                (this.Address == null ? "" : ", Address:" + this.Address);
+        }
+    }
+}
---
+++ PROGRAMMING/PART THREE OOP IN C#/HOMEWORKS/DefiningClasses/Persons/PersonTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class PersonTest
+    {
+        static void Main(string[] args)
+        {
+            Person mitko = new Person("Dimitar", 18, "Town - Ruse");
+            Person rado = new Person("Radostin", 22);
+            Console.WriteLine(mitko);
+            Console.WriteLine(rado);
+        }
+    }
+}
